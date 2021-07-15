c4501349d5141022f2838b7f1fc718ed0bafc23c
solvisig <solvi@ok.is>
2021-07-15 23:45:24 +0000

    instagram feed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import react,{ useEffect, useRef, useState } from 'react';
+import react,{ useEffect, useRef, useState } from 'react'
+import InstagramFeed  from 'react-ig-feed'
+import 'react-ig-feed/dist/index.css'
 import './App.css';
 
 function App() {
-  
+  //token: IGQVJWOG9GbERQeVd3RmtpSC1wNS05Ylg3NmdjajA2d0QtT0RfUDRlV2U5a3dfSTlGTWZApODRmbFZAvZA2F3c0ZAKai01VXFxZAUMyUHR6ME5VcV9lQlBHQ2VzWmw3NHhWX1R4YnZA5aW5BTWlkcUxCUmswSQZDZD
   return (
     <section>
     <section className="timer-container">
@@ -25,8 +27,10 @@ function App() {
           <i className="mdi mdi-youtube" />
           </a>
         </div>
-            
-          
+        <div className="igFeed"> 
+          <p>Instagram stemningin:</p>       
+          <InstagramFeed token="IGQVJXamtxcGdyVjlzemgwdGJHWi1XQ09kRDQ0T2RJOUIzTjVMRno4R0h2Y1ZABMTJsTHZAFRmlMTnk1NmEzSXhEclNEYnVNNk41b1dVTjVqa2JPb0NqMzZAWMld1WEF1TFFxQTFsQW1FM0l0RWsybF96RQZDZD"  counter="6"/>
+        </div>
         </div>
         
         
